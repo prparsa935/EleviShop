86c19fbe60a6b774a8911a43d01656321045fb1e
prparsa <100439233+prparsa935@users.noreply.github.com>
2024-09-10 01:32:55 +0330

    test

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -13,13 +13,17 @@ const AuthProvider = (props) => {
 
   const [cookies, setCookie, removeCookie] = useCookies("access");
   const [registerErrM, setregisterErrM] = useState(null);
-  const [access, setAccess] = useState(null);
+  const [access, setAccess] = useState(
+    cookies?.access ? cookies?.access : null
+  );
   const [shoppingCart, setShoppingCart] = useState(
     JSON.parse(localStorage?.getItem("shoppingCart")) || []
   );
   const count = useRef(false);
   // error here
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(
+    cookies?.access ? jwtDecode(cookies?.access) : null
+  );
   //   const [shoppingCartItems, setShoppingCartItems] = useState(null);
   const url = useLocation();
   // todo not first time
